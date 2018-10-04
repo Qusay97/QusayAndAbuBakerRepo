8827215635dcec38ca97a0864c3198582e0d601f
Ashraf Hasan Alsheikh <alsheikh813@gmail.com>
2018-10-04 11:06:42 +0300

    documented code

--- project.js
+++ project.js
@@ -1,28 +1,33 @@
- function each(coll, func) { 
-       if (Array.isArray(coll)) { 
-             for (var i = 0; i < coll.length; i++) { 
-                   func(coll[i], i); 
-             } 
-       } else { 
-             for (var key in coll) { 
-                   func(coll[key], key); 
-             } 
+// each function for abstraction
+function each(coll, func) { 
+ if (Array.isArray(coll)) { 
+   for (var i = 0; i < coll.length; i++) { 
+         func(coll[i], i); 
+   } 
+ } else { 
+     for (var key in coll) { 
+        func(coll[key], key); 
        } 
- }
+ } 
+}
 
 
 
 
 
-
+//data we use in the website
 var chefs = [
 {name: 'Zaid', food: [ 'Mansaf', 'magluba', 'salat', 'Kabab', 'Mutabal' ]},
 {name: 'Kushi', food: [ 'Chaat', 'Dal', 'Biryni', 'Roti', 'Naan'] },
 {name: 'Umed', food: [ 'Pide', 'Kofta', 'Baklava', 'Kabab', 'Lahmajoun']},
 {name: 'Esra', food: [ 'Houmous', 'Kibbeh', 'Zaattar', 'Sujuk', 'Pastirma']}
 ] 
+
+
 var foods = ['Mansaf', 'magluba', 'salat', 'Kabab', 'Mutabal', 'Chaat', 'Dal', 'Biryni', 'Roti', 'Naan', 
 'Pide', 'Kofta', 'Baklava', 'Kabab', 'Lahmajoun', 'Houmous', 'Kibbeh', 'Zaattar', 'Sujuk', 'Pastirma']
+
+
 var foodtype = [
 {name: 'Jordanian', food: [ 'Mansaf', 'magluba', 'salat', 'Kabab', 'Mutabal' ]},
 {name: 'Indian', food: [ 'Chaat', 'Dal', 'Biryni', 'Roti', 'Naan'] },
@@ -30,6 +35,8 @@ var foodtype = [
 {name: 'Syrian', food: [ 'Houmous', 'Kibbeh', 'Zaattar', 'Sujuk', 'Pastirma']}
 ] 
 
+
+//older code
 // var h3 = $("<h3></h3>")
 // var p = $("<p></p>")
 // var name1 = $("<h3></h3>").text(chefs[0].name)
@@ -49,7 +56,7 @@ var foodtype = [
 // $(p).appendTo('#chefs')
 // }
 
-
+//abstraction function for each
 function eachRefactored(array,variable,variable2,id,tag1,tag2){
 	each(array, function(element,i){
 		var variable = $(tag1).text(array[i].name)
@@ -60,16 +67,16 @@ function eachRefactored(array,variable,variable2,id,tag1,tag2){
 }
 
 
-
+//used refacored each
 eachRefactored(chefs,"chef","p","#chefs","<h3></h3>","<p id='pfood'></p>");
 eachRefactored(foodtype,"type","food","#type","<h3></h3>","<p id='pfood'></p>");
-
+// each for array
 each(foods, function(element,i){
 	var food = $("<p id='pfood'></p>").text(foods[i]);
 	$(food).appendTo("#foods")
 });
 
-
+// older code
 // each(chefs, function(element,i){
 // 	var chef = $("<h3></h3>").text(chefs[i].name)
 // 	$(chef).appendTo('#chefs')
@@ -86,19 +93,20 @@ each(foods, function(element,i){
 // 	var food = $("<p id='pfood'></p>").text("Food: " + foodtype[i].food)
 // 	$(food).appendTo('#type')
 // });
-
+// the fade effect function
 function fade(id, time){
 	$(id).hide()
 setTimeout(function(){
 	$(id).show()
 },time)
 }
-
+// calling the function
 fade('#intro', 1000)
 fade('#chefs', 2000)
 fade('#foods', 3000)
 fade('#type', 4000)
 
+//older code
 // $('#intro').hide()
 // setTimeout(function(){
 // 	$('#intro').show()
@@ -118,10 +126,10 @@ fade('#type', 4000)
 // setTimeout(function(){
 // 	$('#type').show()
 // },4000)
-
+//hiding the elements
 $("#input").hide()
 $("#btn").hide()
-
+//making the button function
 var btn2 = $("<button id='btn2'>Order From Us</button>");
 btn2.click(function(){
 	$("#input").show()
@@ -130,6 +138,7 @@ btn2.click(function(){
 })
 btn2.appendTo("#btn3")
 
+//the order function for the button
 function order(){
 	var input = $("#input").val()
 	alert(input + " will be ready to be delivered in 30 minutes")
